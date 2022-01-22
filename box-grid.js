d4635461eaa63ca1ff57c17f17d9f9c236dcd3e0
Endonist <65572227+roddians@users.noreply.github.com>
2022-01-22 17:05:24 +0700

    Update box-grid.js
    tambah floating sidebar

--- box-grid.js
+++ box-grid.js
@@ -336,3 +336,35 @@
 	};
 	
 } )( jQuery, window );
+
+
+
+//sidebar floating
+
+$(window).scroll(function(){
+var wScroll = $(this).scrollTop();
+	
+	$('.item-view .sidebar-container').css({
+		'transform' : 'translate(0px,'+ wScroll/2 +'%)'
+	
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
